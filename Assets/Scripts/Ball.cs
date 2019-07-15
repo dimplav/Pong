e79ade7454fe3431f5904f23e4f2290fc96ff1e3
dimplav <plavsicdimitrije@gmail.com>
2019-07-15 12:51:51 +0200

    added left,top and right colliders, game starts on mouse click

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    bool hasStarted = false;
+    Vector2 paddleToBallVector;
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted) {
+            LockBallToPaddle();
+            LaunchBallOnMouseClick();
+        }
+    }
+
+    private void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minPaddle = 0f;
+    [SerializeField] float maxPaddle = 16f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition);
+        float xPosMouse = Input.mousePosition.x/Screen.width * screenWidthInUnits;
+        Vector2 PaddlePos = new Vector2(transform.position.x, transform.position.y);
+        PaddlePos.x = Mathf.Clamp(xPosMouse, minPaddle+1, maxPaddle-1);
+        transform.position = PaddlePos;
     }
 }
